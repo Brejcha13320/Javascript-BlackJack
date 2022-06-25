51258fd9478745cbdcc2cb7e2edb674e06b85aa1
Brejcha13320 <davidmejia-2000@outlook.com>
2022-06-25 17:02:25 -0500

    subiendo el proyecto completado y los docs para github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-import { saludar } from './js/componentes';
 import './styles.css';
 
-const nombre = 'Fernando';
+import { Todo, TodoList } from './classes';
+import { crearTodoHTML } from './js/componentes';
 
-saludar( nombre );
+export const todoList = new TodoList();
+
+/* Forma Larga */
+todoList.todos.forEach(todo => crearTodoHTML(todo));
+
+/* Forma Corta */
+// todoList.todos.forEach(crearTodoHTML);
+
+
